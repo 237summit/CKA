705abfda28e20ecc67e9c22192c0eba6ab4ef337
SEONGMI LEE <42544305+237summit@users.noreply.github.com>
2022-09-30 07:09:03 +0900

    Update lab-k8s_cka.sh

--- lab-k8s_cka.sh
+++ lab-k8s_cka.sh
@@ -353,6 +353,11 @@ sudo mv etcd etcdctl etcdutl /usr/local/bin
 etcd --version
 cd
 
+sudo ETCDCTL_API=3 etcdctl --endpoints=https://127.0.0.1:2379 \
+   --cacert=/etc/kubernetes/pki/etcd/ca.crt \
+   --cert=/etc/kubernetes/pki/etcd/server.crt \
+   --key=/etc/kubernetes/pki/etcd/server.key \
+   snapshot save //data/etcd-snapshot-previous.db
 
 ===END==
 
